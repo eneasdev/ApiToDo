008904cae62613767b2de167023ec9924eb30930
Eneas <lucasmartins_eu@hotmail.com>
2020-10-30 15:27:24 -0300

    Validações nas Requisicões feitas

--- ToDo/ToDo/Controllers/ToDoController.cs
+++ ToDo/ToDo/Controllers/ToDoController.cs
@@ -20,38 +20,59 @@ namespace ToDo.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok(_tarefaRepository.Get());
+            if(_tarefaRepository.Get() == null || _tarefaRepository.Get().Count == 0)
+                return NotFound(
+                    "Não existe tarefas."
+                    );
+            else
+                return Ok(_tarefaRepository.Get());
         }
 
         [Route("api/[controller]")]
         [HttpPost]
         public IActionResult Adicionar([FromBody]Tarefa tarefa)
         {
-            _tarefaRepository.Adicionar(tarefa);
-            return Ok(tarefa);
+            if(tarefa == null)
+                return NotFound(
+                    "Insira uma tarefa."
+                    );
+            else
+                _tarefaRepository.Adicionar(tarefa);
+                return Ok(tarefa);
         }
 
         [Route("api/[controller]/{id}")]
         [HttpGet]
         public IActionResult Pegar([FromRoute]int id)
         {
-            return Ok(_tarefaRepository.Pegar(id));
+            if (id <= 0)
+                return NotFound(
+                    "Tarefa não existe"
+                    );
+            else
+                return Ok(_tarefaRepository.Pegar(id));
         }
 
         [Route("api/[controller]/{id}")]
         [HttpDelete]
         public IActionResult Deletar([FromRoute] int id)
         {
-            _tarefaRepository.Deletar(id);
-            return Ok();
+            if (id != 0)
+                _tarefaRepository.Deletar(id);
+                return Ok();
         }
 
         [Route("api/[controller]")]
         [HttpPatch]
         public IActionResult Atualizar([FromBody] Tarefa tarefa)
         {
-            _tarefaRepository.Atualizar(tarefa);
-            return Ok();
+            if(tarefa == null)
+                return NotFound(
+                    "Tarefa não encontrada"
+                    );
+            else
+                _tarefaRepository.Atualizar(tarefa);
+                return Ok();
         }
     }
 }
